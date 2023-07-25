9d8e9e62b74f0168c586633621e81e7453a2fbe0
Reyna May <roma.reynamayA@gmail.com>
2023-07-24 18:13:00 -0600

    Modify the query to use HAVING

--- 0_selects/5_students_total_submissions.sql
+++ 0_selects/5_students_total_submissions.sql
@@ -3,4 +3,5 @@ COUNT(assignment_submissions.*) AS total_submissions
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.end_date IS NULL
-GROUP BY student;
+GROUP BY students.name
+HAVING COUNT(assignment_submissions.*) < 100;
