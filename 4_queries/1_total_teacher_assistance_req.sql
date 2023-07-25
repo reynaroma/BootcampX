10d1e6a9418e38fe5ad06c17b3bd3d2f8f2a0e43
Reyna May <roma.reynamayA@gmail.com>
2023-07-25 14:53:25 -0600

    Correct the typo from the alias

--- 4_queries/1_total_teacher_assistance_req.sql
+++ 4_queries/1_total_teacher_assistance_req.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(assistance_requests) AS total_assitances,
+SELECT COUNT(assistance_requests) AS total_assistances,
   teachers.name AS name
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
