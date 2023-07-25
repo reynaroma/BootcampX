7458079cdf6a5e2c472abf4ac4480a3b53154cc7
Reyna May <roma.reynamayA@gmail.com>
2023-07-25 15:22:57 -0600

    Add query for average cohort assistance time

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,7 @@
+SELECt cohorts.name AS name,
+  AVG(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY AVG(completed_at - started_at);
